59cb6efb0c64ea21d00a5c40d3bcebe5f14ea322
AinaEmmanuel <ainae06@gmail.com>
2021-03-05 15:57:03 +0100

    deleted cache

--- create_db.py
+++ create_db.py
@@ -1,3 +1,9 @@
+from csflask import create_app
 from csflask import db
+from csflask.models import current_app
+from csflask.__init__ import db
+app = create_app()
+app.app_context().push()
 
+print(app)
 db.create_all()
