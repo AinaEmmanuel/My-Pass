0f7c6347d4ec9a3a4d2bbf9388f068ab4254a5db
AinaEmmanuel <ainae06@gmail.com>
2021-03-05 16:17:09 +0100

    postgres db

--- create_db.py
+++ create_db.py
@@ -1,9 +1,5 @@
 from csflask import create_app
-from csflask import db
-from csflask.models import current_app
-from csflask.__init__ import db
 app = create_app()
 app.app_context().push()
-
-print(app)
+from csflask import db
 db.create_all()
