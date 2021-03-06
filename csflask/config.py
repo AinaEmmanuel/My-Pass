c4b081ecda0743a0e1b6c90b0f28acfbda900c7c
AinaEmmanuel <ainae06@gmail.com>
2021-03-06 13:19:53 +0100

    change config to os

--- csflask/config.py
+++ csflask/config.py
@@ -2,10 +2,10 @@ import os
 from decouple import config
 
 class Config:
-    SECRET_KEY = config('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = config('POSTGRES_DATABASE_URL')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('POSTGRES_DATABASE_URL')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = config('USERNAME')
-    MAIL_PASSWORD = config('PASSWORD')
+    MAIL_USERNAME = os.environ.get('USERNAME')
+    MAIL_PASSWORD = os.environ.get('PASSWORD')
