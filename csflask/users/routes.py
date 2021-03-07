c89d78c462711c229dd1d3d4dedeaa91a0610d12
AinaEmmanuel <ainae06@gmail.com>
2021-03-07 09:35:03 +0100

    login pass in lowercase

--- csflask/users/routes.py
+++ csflask/users/routes.py
@@ -57,7 +57,8 @@ def login():
         return redirect(url_for('main.mypasswords'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
+        email=form.email.data
+        user = User.query.filter_by(email=email.lower()).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
